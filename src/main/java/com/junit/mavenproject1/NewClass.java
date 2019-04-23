5f2decdfb4d9493e09c9bbd7ea06dfe869396be7
aprPrasanth <apr.prasanth@gmail.com>
2019-04-23 08:00:44 +0530

    Add existing file

---
+++ src/main/java/com/junit/mavenproject1/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.junit.mavenproject1;
+
+/**
+ *
+ * @author prasanth
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Yes");
+    }
+}
